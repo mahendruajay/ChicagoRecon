c0e832e8fe3e1b0e143f7d9eb870c630cba252ea
Ryan Ellison <rellison@rellison.duncllc.com>
2015-11-30 19:19:41 -0600

    fixing issues

--- src/main/java/service/AirportService.java
+++ src/main/java/service/AirportService.java
@@ -1,8 +1,6 @@
 package service;
 
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Service;
 
 /**
  * Created by rellison on 11/30/15.
