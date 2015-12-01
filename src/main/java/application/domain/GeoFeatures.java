f7105b6c6e92065dfa629f64048c3b947b93aad9
amahendru <amahendru@expedia.com>
2015-12-01 14:47:40 -0600

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	pom.xml

---
+++ src/main/java/application/domain/GeoFeatures.java
@@ -0,0 +1,11 @@
+package application.domain;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+/**
+ * Created by amahendru on 12/1/15.
+ */
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class GeoFeatures {
+
+}
