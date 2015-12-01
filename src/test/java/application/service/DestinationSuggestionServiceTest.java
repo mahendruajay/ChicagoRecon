a6db9b9f4a25b25aaac00f0573772d7ec53d708b
Ryan Ellison <rellison@rellison.duncllc.com>
2015-12-01 15:28:18 -0600

    add test for suggestion service

--- src/test/java/application/service/DestinationSuggestionServiceTest.java
+++ src/test/java/application/service/DestinationSuggestionServiceTest.java
@@ -1,10 +1,32 @@
 package application.service;
 
+import org.junit.Before;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+
 import static org.junit.Assert.*;
 
 /**
  * Created by rellison on 12/1/15.
  */
 public class DestinationSuggestionServiceTest {
+
+    DestinationSuggestionService service;
+
+    private final Logger log = LoggerFactory.getLogger(this.getClass());
+
+    @Before
+    public void setup(){
+        service = new DestinationSuggestionService();
+    }
+
+    @Test
+    public void testService(){
+
+        String result = service.getNextDestinationTesting();
+        log.info(result);
+    }
 
 }
