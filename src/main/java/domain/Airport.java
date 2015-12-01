c0b2ee1412de581ac37a788a2c45abf829f766a0
amahendru <amahendru@expedia.com>
2015-11-30 19:15:59 -0600

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	src/main/java/controller/ApiController.java

---
+++ src/main/java/domain/Airport.java
@@ -0,0 +1,25 @@
+package domain;
+
+/**
+ * Created by amahendru on 11/30/15.
+ */
+public class Airport {
+    private String code;
+    private String city;
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+}
