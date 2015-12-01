4d15ab184fc2ba0e7d9ea360965f197006dc9ea2
amahendru <amahendru@expedia.com>
2015-11-30 19:51:10 -0600

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	src/main/java/controller/ApiController.java

--- src/main/java/service/AirportService.java
+++ src/main/java/service/AirportService.java
@@ -1,10 +1,19 @@
 package service;
 
+import com.sun.jersey.api.client.Client;
+import com.sun.jersey.api.client.WebResource;
+import org.springframework.stereotype.Component;
+
 /**
  * Created by rellison on 11/30/15.
  */
+@Component
 public class AirportService {
+
     public String getAirportCode(Double latitude, Double longitude) {
+
+        Client client = Client.create();
+        WebResource webResource = client.resource("");
         return "ORD";
     }
 }
