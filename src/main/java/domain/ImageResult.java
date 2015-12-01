47578788dd8a90fa4ece326a8774e7eb3c31c98a
pmredding <predding@expedia.com>
2015-12-01 08:57:10 -0600

    src

---
+++ src/main/java/domain/ImageResult.java
@@ -0,0 +1,31 @@
+package domain;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+/**
+ * Created by predding on 11/30/15.
+ */
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class ImageResult {
+    @JsonProperty("width")
+    int width;
+
+    @JsonProperty("height")
+    int height;
+
+    @JsonProperty("url")
+    String url;
+
+    public int getWidth() {
+        return width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+}
