7f891a2006eefc6557a636dc3f5160f82402cffb
amahendru <amahendru@expedia.com>
2015-12-01 18:21:52 -0600

    Fill in code for getFlight

--- src/main/java/application/controller/ApiController.java
+++ src/main/java/application/controller/ApiController.java
@@ -2,19 +2,17 @@ package application.controller;
 
 import application.domain.*;
 import application.service.*;
-import com.google.common.collect.Lists;
-import org.joda.time.LocalDate;
-import org.joda.time.format.DateTimeFormat;
-import org.joda.time.format.DateTimeFormatter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 
 @RestController
 public class ApiController {
 
-    private static final DateTimeFormatter DATE_FORMAT = DateTimeFormat.forPattern("yyyy-MM-dd");
+    private static final DateTimeFormatter DATE_FORMAT = DateTimeFormatter.ISO_DATE;
 
     @Autowired
     private AirportService airportService;
@@ -31,6 +29,9 @@ public class ApiController {
     @Autowired
     private CruiseSuggestionService cruiseSuggestionService;
 
+    @Autowired
+    DestinationSuggestionService destinationSuggestionService;
+
     @RequestMapping("/")
     public String index() {
         return "Greetings from Spring Boot!";
@@ -44,21 +45,29 @@ public class ApiController {
     }
 
     @RequestMapping(value = "/api/suggestion", method = RequestMethod.GET)
-    public FlightSuggestion getFlight(@RequestParam("user") String user,
+    public FlightSuggestion getFlight(@RequestParam("user") String userID,
                                       @RequestParam("departureCity") String departureCity,
+                                      @RequestParam("departureCity") String departureAirportCode,
                                       @RequestParam("date") String date) {
 
         // The imageService and cruiseSuggestionService calls should happen from withing the flightSuggestionService, but for now are here
         LocalDate departureDate = LocalDate.parse(date, DATE_FORMAT);
         LocalDate returnDate = departureDate.plusDays(7);
 
+        Suggestion suggestion = destinationSuggestionService.getNextDestination(userID, departureCity);
+
+        String depDate = departureDate.format(DATE_FORMAT);
+        String retDate = returnDate.format(DATE_FORMAT);
+
+        Flights flights = flightSearchService.getFlights(depDate, departureAirportCode, suggestion.getAirportCode(), retDate);
+
         DestinationDetails destinationDetails = new DestinationDetails();
-        destinationDetails.setDestinationImages(imageService.getCityImages("Miami", Lists.newArrayList("beach"), 3));
+        destinationDetails.setDestinationImages(imageService.getCityImages(suggestion.getCityName(), suggestion.getLabels(), 3));
 
-        CruiseSuggestion cruiseSuggestion = cruiseSuggestionService.getCruiseSuggestion("Miami", departureDate, returnDate);
+        CruiseSuggestion cruiseSuggestion = cruiseSuggestionService.getCruiseSuggestion(suggestion.getAirportCode(), departureDate, returnDate);
 
-        return new FlightSuggestion(BigDecimal.valueOf(100), "2016-01-01",
-                new Airport("CHI", "Chicago, IL"), new Airport("MIA", "Miami, FL"), destinationDetails, cruiseSuggestion);
+        return new FlightSuggestion(flights.getPrice(), flights.getDepartureDate(),
+                new Airport(departureAirportCode, departureCity), new Airport(suggestion.getAirportCode(), suggestion.getCityName()), destinationDetails, cruiseSuggestion);
     }
 
     @RequestMapping("/api/suggestion/getAirportInfo")
--- src/main/java/application/service/CruiseSuggestionService.java
+++ src/main/java/application/service/CruiseSuggestionService.java
@@ -4,16 +4,15 @@ import application.domain.CruiseSuggestion;
 import application.util.CommonUtils;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.joda.time.Days;
-import org.joda.time.LocalDate;
-import org.joda.time.format.DateTimeFormat;
-import org.joda.time.format.DateTimeFormatter;
 import org.json.JSONArray;
 import org.json.JSONObject;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.time.LocalDate;
+import java.time.Period;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
@@ -26,15 +25,15 @@ import java.util.Map;
 public class CruiseSuggestionService {
     private static final String CRUISE_SEARCH_OPTIONS_URL = "https://www.expedia.com/cruiseSearchCriteria/getSearchOptions?earliest-departure-date=%s&latest-departure-date=%s&departure-port=%s&min-length=1&max-length=%d";
     private static final String CRUISE_SEARCH_DEEP_LINK_URL = "https://www.expedia.com/Cruise-Search?earliest-departure-date=%s&latest-departure-date=%s&departure-port=%s&min-length=1&max-length=%d";
-    private static final DateTimeFormatter DATE_FORMAT = DateTimeFormat.forPattern("yyyy-MM-dd");
+    private static final DateTimeFormatter DATE_FORMAT = DateTimeFormatter.ISO_DATE;
 
     private static Map<String, String> airportCodeToDeparturePortMap = null;
 
     public CruiseSuggestion getCruiseSuggestion(String city, LocalDate departureDate, LocalDate returnDate) {
         LocalDate earliestDepartureDate = departureDate.plusDays(1);
         LocalDate latestDepartureDate = returnDate.minusDays(2);
-        String earliestDeparture = DATE_FORMAT.print(earliestDepartureDate);
-        String latestDeparture = DATE_FORMAT.print(latestDepartureDate);
+        String earliestDeparture = earliestDepartureDate.format(DATE_FORMAT);
+        String latestDeparture = latestDepartureDate.format(DATE_FORMAT);
 
         try {
             String departurePort = getDeparturePort(city);
@@ -42,7 +41,7 @@ public class CruiseSuggestionService {
                 return null;
             }
 
-            int maxLength = Days.daysBetween(earliestDepartureDate, latestDepartureDate).getDays();
+            int maxLength = Period.between(earliestDepartureDate, latestDepartureDate).getDays();
 
             String searchOptionsUrl = String.format(Locale.ENGLISH, CRUISE_SEARCH_OPTIONS_URL, earliestDeparture, latestDeparture, departurePort, maxLength);
             String response = CommonUtils.readJsonFromUrl(searchOptionsUrl);
